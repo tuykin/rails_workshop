90d5f54dfe4987de81ad1df9d07375e1c3e2ecec
Anvar Tuykin <tuykin@gmail.com>
2018-02-03 21:06:16 +0300

    Add Article::Category#name uniqueness constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203141717) do
+ActiveRecord::Schema.define(version: 20180203180339) do
 
   create_table "article_categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_article_categories_on_name", unique: true
   end
 
   create_table "article_comments", force: :cascade do |t|

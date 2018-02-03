e9c9f79a4e70ed42c82b9d9fa59eda471de1395a
Anvar Tuykin <tuykin@gmail.com>
2018-02-03 12:10:53 +0300

    Add resource_article and requested_article

--- app/controllers/web/articles/application_controller.rb
+++ app/controllers/web/articles/application_controller.rb
@@ -1,3 +1,11 @@
 class Web::Articles::ApplicationController < Web::ApplicationController
   protect_from_forgery with: :exception
+
+  def resource_article
+    requested_article || Article.new
+  end
+
+  def requested_article
+    Article.find(params[:article_id])
+  end
 end
